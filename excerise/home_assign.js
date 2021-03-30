da73ceb6ff7c0119f52396866a7288171c6b8d27
vignesh <vigneshachari06@gmail.com>
2021-03-30 13:55:56 +0530

    updated code

--- excerise/home_assign.js
+++
@@ -1,9 +0,0 @@
-function checkLeapYear(year) {
-   if ((0 == year % 4 && 0 != year % 100) || 0 == year % 400){
-   console.log(`${year} is a leap year`)
-   } else {
-     console.log(`${year} isn't a leap year`)
-   }
-} 
-
-checkLeapYear(2020)
